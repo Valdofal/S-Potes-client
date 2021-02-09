4816ac375a23b8008649ad0aa0ccbd52f0645d04
nath-btx <nathan.bertiaux@hotmail.fr>
2021-02-09 14:58:00 +0100

    formulaire presque finis -> ajouter headers

--- src/Formulaire.js
+++ src/Formulaire.js
@@ -6,11 +6,13 @@ function Formulaire() {
       <form action="localhost:3000/user" method="post">
         <input class="box" type="email" name="email" id="email" placeholder="Adresse e-mail" required /> <br/>
         <input class="box" type="password" name="password" id="password" placeholder="Mot de passe" required /> <br/>
-        <input class="box" type="text" name="firstName" id="firstName" placeholder="Rrénom" required /><br/>
+        <input class="box" type="text" name="firstName" id="firstName" placeholder="Prénom" required /><br/>
         <input class="box" type="text" name="lastName" id="lastName" placeholder="Nom" required /> <br/>
-        <input class="box" type="date" name="birthdate" id="birthdate" placeholder="dd/mm/aaaa" required /> <br/>
-        <input type="submit" id="submitDetails" name="submitDetails" value = "S'inscrire "/>
+        <input class="box" type="date" name="birthdate" id="birthdate" placeholder="jj/mm/aaaa" required /> <br/>
+        <p> En créant un compte, vous acceptez de vous conformer à la <a> Politique de confidentialité </a>et aux <a>Conditions Générales</a></p>
+        <input type="submit" id="submitDetails" name="submitDetails" value = "S'INSCRIRE" class="button"/>
       </form>
+      <p class="dejamembre"> Déjà membre ? <a> Se connecter </a> </p>
     </div>
     )
   }
